17a319e6ac0c93295f4b3b1be81cb401262d6ccd
hredan <hredan@sourcecode3d.de>
2024-06-09 20:10:18 +0000

    build script: create data directory

--- build.sh
+++ build.sh
@@ -1,1 +1,18 @@
-cd /home/workspace/ESP_TOGGLE_LED_ANGULAR
+#!/bin/bash
+SCRIPT_DIR=${0%/*}
+OUTPUT_DIR=$SCRIPT_DIR/ESP_Backend/data
+echo $SCRIPT_DIR
+cd $SCRIPT_DIR/ESP_Angular_Frontend
+ng build
+retVal=$?
+if [ $retVal -ne 0 ]; then
+        echo "Error in ng build"
+        exit $retVal
+fi
+# remove data directory
+rm -rf $OUTPUT_DIR
+cd ..
+cp -r $SCRIPT_DIR/ESP_Angular_Frontend/dist/my-app/browser $OUTPUT_DIR
+
+
+
