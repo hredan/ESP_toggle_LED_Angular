2865fef2af44ea5b926d22677e2ed26efd97f3ff
hredan <hredan@sourcecode3d.de>
2025-02-24 21:06:18 +0100

    use eep-name: 'ESP_toggle_LED_Angular'

--- Scripts/create_eep.sh
+++ Scripts/create_eep.sh
@@ -7,3 +7,7 @@ PATH=/home/workspace/eef-carefree-action/:$PATH
 PATH=/home/workspace/eep-build-action/:$PATH
 
 build_sketch.sh -s ESP_toggle_LED_Backend -c esp8266 -b d1_mini
+GIT_URL=$(git config --get remote.origin.url)
+GIT_COMMIT=$(git rev-parse HEAD)
+GIT_TAG_BRANCH=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --abbrev-ref HEAD)@local
+create_build_info.sh -r $GIT_URL -s $GIT_COMMIT -n $GIT_TAG_BRANCH
